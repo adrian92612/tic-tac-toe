159a3a05d5e4e90ad1f9bf0e4c7b1144719b31ff
adrian92612 <adrianvillamin0612@gmail.com>
2023-05-09 13:11:39 +0800

    test commit

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ function CreatePlayer(name, mark) {
   return { name, mark };
 }
 
-function GameBoard() {}
+const GameBoard = (() => {
+  let boxes = Array.from(document.getElementsByClassName("box"));
 
-function Game() {}
+  const getBoxes = () => boxes;
+  return {
+    getBoxes,
+  };
+})();
+
+const Game = (() => {
+  //initialize player data
+  const start = () => {};
+  return {
+    start,
+  };
+})();
 
 const startButton = document.getElementById("start-button");
 startButton.addEventListener("submit", () => Game.start());
