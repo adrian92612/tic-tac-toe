1029237465e460c815380ae2a629fec62c72e557
adrian92612 <adrianvillamin0612@gmail.com>
2023-05-10 17:09:48 +0800

    finalize the logic of the game, might add AI later

--- main.js
+++ main.js
@@ -1,9 +1,10 @@
-function CreatePlayer(name, mark) {
-  return { name, mark };
+function CreatePlayer(name) {
+  return name;
 }
 
 const GameBoard = (() => {
-  let boxes = Array.from(document.getElementsByClassName("box"));
+  let boxes = Array.from(document.querySelectorAll(".box"));
+
   boxes.forEach((box) =>
     box.addEventListener("click", (box) => Game.handleClick(box), {
       once: true,
@@ -11,41 +12,84 @@ const GameBoard = (() => {
   );
   const form = document.getElementById("form");
   form.addEventListener("submit", (e) => Game.start(e));
-  const resetBoard = () => window.location.reload();
+
+  const displayWinner = (winner, draw) => {
+    const resultBoard = document.querySelector("#result-board");
+    const resetBtn = document.querySelector("#reset");
+    let message = "";
+    draw ? (message = "Draw!!!") : (message = `${winner} is the winner!`);
+    resetBtn.addEventListener("click", () => window.location.reload());
+    resultBoard.textContent = message;
+    document.querySelector(".result").classList.add("show");
+  };
   const getBoxes = () => boxes;
   return {
-    resetBoard,
     getBoxes,
+    displayWinner,
   };
 })();
 
 const Game = (() => {
-  //initialize player data
+  const WIN_COMBINATIOIN = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  //*****initialize player data
   let playerX;
   let playerO;
   let playerXturn = true;
 
   const start = (e) => {
     e.preventDefault();
-    playerX = CreatePlayer(document.getElementById("player-x").value, "X");
-    playerO = CreatePlayer(document.getElementById("player-o").value, "O");
+    playerX = CreatePlayer(document.getElementById("player-x").value);
+    playerO = CreatePlayer(document.getElementById("player-o").value);
     e.target.classList.add("hide");
   };
 
-  const checkWinner = () => {
-    console.log(GameBoard.getBoxes());
+  const checkDraw = () => {
+    return GameBoard.getBoxes().every((box) => {
+      return box.classList.contains("x") || box.classList.contains("o");
+    });
+  };
+
+  const checkWinner = (mark) => {
+    return WIN_COMBINATIOIN.some((combination) => {
+      return combination.every((index) => {
+        return GameBoard.getBoxes()[index].classList.contains(mark);
+      });
+    });
   };
 
   const handleClick = (box) => {
-    console.log(box.target);
-    playerXturn ? box.target.classList.add("x") : box.target.classList.add("o");
-    //check win/lose/draw
+    if (playerXturn) {
+      box.target.classList.add("x"); //add marker
+      box.target.parentElement.classList.add("o"); //  display placeholder marker for next player
+      box.target.parentElement.classList.remove("x");
+      if (checkWinner("x")) {
+        GameBoard.displayWinner(playerX);
+        return;
+      }
+    } else {
+      box.target.classList.add("o");
+      box.target.parentElement.classList.add("x");
+      box.target.parentElement.classList.remove("o");
+      if (checkWinner("o")) {
+        GameBoard.displayWinner(playerO);
+        return;
+      }
+    }
+    if (checkDraw()) GameBoard.displayWinner(null, true);
     playerXturn = !playerXturn;
   };
 
   return {
     handleClick,
     start,
-    checkWinner,
   };
 })();
